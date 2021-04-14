fb94a67150fdb66477d79a9a8f35be7427ac4bd5
Icebrave <71671391+Icebrave@users.noreply.github.com>
2021-04-13 23:02:08 -0400

    .

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
 console.log("connected");
+
+const questions = [
+  {
+    question: "What color are most turtles?",
+    choices: {
+      a: "Yellow",
+      b: "Green",
+      c: "Purple",
+      d: "White",
+    },
+    answer: "Green",
+  },
+  {
+    question: "What do turtles not eat?",
+    choices: {
+      a: "Jellyfish",
+      b: "Seaweed",
+      c: "Plastic",
+      d: "Fruits",
+    },
+    answer: "Plastic",
+  },
+  {
+    question: "How long do sea turtles live?",
+    choices: {
+      a: "1 Year",
+      b: "5 Years",
+      c: "50 Years",
+      d: "100+ Years",
+    },
+    answer: "Plastic",
+  },
+];
+
+const choices = document.getElementsByClassName("choices");
